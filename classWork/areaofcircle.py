6e8b71ed213308c45c3f170241033250d45ee116
Moses Oghene <mosesogheneo@gmail.com>
2024-10-17 16:52:33 +0100

    ClassWork areaOfCircle and invert3 digit Number program

---
+++ classWork/areaofcircle.py
@@ -0,0 +1,3 @@
+radius = float(input("Enter radius >> "))
+area = 3.142 * (radius ** 2 )
+print("Area is: ", area)
---
+++ classWork/invertnumber.py
@@ -0,0 +1,10 @@
+data = int(input("enter number between 100 - 999 >>"))
+
+if data > 99 and data < 1000:
+    last_num = data % 10
+    temp_data = data // 10
+    mid_num = temp_data % 10
+    first_num = temp_data // 10
+    print( last_num, mid_num, first_num)
+else:
+    print(f"{data} is out of range!")
