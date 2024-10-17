8644142591281be7da7e932b44b4981557487e32
Moses Oghene <mosesogheneo@gmail.com>
2024-10-17 18:13:39 +0100

    Exercise 2.5

---
+++ exercise/chapterTwo/circleareadiametercircumference.py
@@ -0,0 +1,10 @@
+"""
+For circle with radius: 2
+"""
+radius = 2
+pi = 3.14159
+area = pi * (radius ** 2)
+diameter = 2 * radius
+circumference = 2* pi * radius
+
+print(f"FOR CIRCLE WITH RADIUS OF 2 \nArea: {area} \nDiameter: {diameter} \nCircumference: {circumference}")
