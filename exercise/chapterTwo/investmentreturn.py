8a16f58c9c2367ed472398e9dc225ca0e05f551f
Moses Oghene <mosesogheneo@gmail.com>
2024-10-17 20:49:17 +0100

    Exercise 2.12

---
+++ exercise/chapterTwo/investmentreturn.py
@@ -0,0 +1,13 @@
+p = 1000
+r = 7
+tenth_year = p * (1 + 7) ** 10 
+thirtieth_year = p * (1 + 7) ** 30
+twentieth_year = p * (1 + 7) ** 20
+
+print(f"""
+In 10 years >> {tenth_year}
+In 20 years >> {twentieth_year}
+In 30 years >> {thirtieth_year}
+""")
+
+
