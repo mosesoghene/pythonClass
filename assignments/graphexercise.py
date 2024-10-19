67006e141da6b66e5a004b3af87a433dd16f4ab5
Moses Oghene <mosesogheneo@gmail.com>
2024-10-19 22:56:30 +0100

    Graph assigment exercise

---
+++ assignments/graphexercise.py
@@ -0,0 +1,10 @@
+graph_lengths = []
+
+loop_counter = 1
+while loop_counter <= 5:
+    user_number_input = int(input("only enter positive integers >> "))
+    graph_lengths.append(user_number_input)
+    loop_counter += 1
+
+for graph_data in graph_lengths:
+    print(graph_data, ":", "*" * graph_data)
