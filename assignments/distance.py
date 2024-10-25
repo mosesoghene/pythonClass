c2db9d34ffa1ad8be1ec29502677cd5644f24e05
Moses Oghene <mosesogheneo@gmail.com>
2024-10-25 06:46:56 +0100

    Distance between two points exercise

--- assignments/distance.py
+++ assignments/distance.py
@@ -1,12 +1,16 @@
-from math import radians, sin, cos, acos
+import math
 
-latitude_one = float(input("Enter latitude 1: "))
-latitude_two = float(input("Enter latitude 2: "))
-longitude_one = float(input("Enter longitude 1: "))
-longitude_two = float(input("Enter longitude 2: "))
+latitude_one = float(input("input the latitude of coordinate 1: "))
+longitude_one = float(input("input the longitude of coordinate 1: "))
+latitude_two = float(input("input the latitude of coordinate 2: "))
+longitude_two = float(input("input the longitude of coordinate 2: "))
+
+latitude_one = math.radians(latitude_one)
+longitude_one = math.radians(longitude_one)
+latitude_two = math.radians(latitude_two)
+longitude_two = math.radians(longitude_two)
 
 EARTH_RADIUS = 6371.01
-distance = EARTH_RADIUS * acos(sin(radians(latitude_one)) * sin(radians(longitude_one)) + cos(radians(latitude_one)) * cos(radians(longitude_one)) * cos(radians(longitude_one) - radians(longitude_two)))
+distance = EARTH_RADIUS * math.acos((math.sin(latitude_one) * math.sin(latitude_two)) + math.cos(latitude_one) * (math.cos(latitude_two) * math.cos(longitude_one - longitude_two)) )
 
-print(distance)
-
+print(f"the distance between those points is: {distance:.13f} km")
