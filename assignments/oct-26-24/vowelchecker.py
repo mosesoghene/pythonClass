09db817b03446f61c091c24b95b5625ea735f76f
mosesoghene <mosesogheneo@gmail.com>
2024-10-27 08:10:52 +0100

    Vowel checker program

---
+++ assignments/oct-26-24/vowelchecker.py
@@ -0,0 +1,13 @@
+user_input = input("Input an alphabet >> ")
+vowels = "aeiou"
+
+if user_input.isalpha():
+    if len(user_input) == 1:
+        if user_input in vowels:
+            print("Input letter is a vowel")
+        else:
+            print("input letter is a consonant")
+    else:
+        print(f"'{user_input}' is more than one letter. Expected 1 alphabet letter")
+else:
+    print(f"'{user_input}' contains a non-alpahbet letter")
