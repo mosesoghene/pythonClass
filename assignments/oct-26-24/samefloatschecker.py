fe7dabd0a27ea8820e24e0483073e0167a2708e6
mosesoghene <mosesogheneo@gmail.com>
2024-10-27 08:36:18 +0100

    same float checker program

---
+++ assignments/oct-26-24/samefloatschecker.py
@@ -0,0 +1,6 @@
+first_input = float(input("Enter a decimal digit >> "))
+second_input = float(input("Enter another decimal digit >> "))
+
+response = "They are same" if round(first_input, 3) == round(second_input, 3) else "They are different"
+
+print(response)
