acb17596ce579e7ff41812422c42dbbb54a52fb1
Moses Oghene <mosesogheneo@gmail.com>
2024-12-02 14:45:59 +0100

    Phase Gate - Task 1 Python - pseudocode

--- phaseGateOne/practiceaddition.py
+++ phaseGateOne/practiceaddition.py
@@ -1,3 +1,15 @@
+"""
+generate random number (1 - 100)
+save to variable first_number
+generate random number (1 - 100)
+save to variable first_number
+
+declare function check_answer with params: user_input, first_number, second_number
+    check if user_input is a valid number
+    check if user_input == first_number + second_number
+    function should return True if above check is true else False
+"""
+
 from random import randint
 
 first_number = randint(0, 100)
