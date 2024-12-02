5879faafd259877ab10d0c73ff57a2a1ac20793f
Moses Oghene <mosesogheneo@gmail.com>
2024-12-02 14:45:33 +0100

    Phase Gate - Task 2 Python - pseudocode

--- phaseGateOne/sumdigitsofnumber.py
+++ phaseGateOne/sumdigitsofnumber.py
@@ -1,3 +1,9 @@
+"""
+prompt user to enter number between 0 and 1000
+save input as user_input (type string type)
+check if user_input is a valid digit and is less than 1000 and greater than 0
+map of user_input and cast each character to int type, sum the numbers
+"""
 user_input = input("Enter a number between 0 and 1000 \n> ")
 
 def sum_digits_of(number):
